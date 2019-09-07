98abb7e93f23c3887915fdaf1ad3e558136caf1e
sshallow <doit_2017@163.com>
2019-09-07 21:57:46 +0800

    Initial commit

---
+++ Example/SlidingSelector/SSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSAppDelegate.h
+//  SlidingSelector
+//
+//  Created by sshallow on 09/07/2019.
+//  Copyright (c) 2019 sshallow. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
