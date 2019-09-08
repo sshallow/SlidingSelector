8b6ce161226b0533d35841696c151866c172336c
shuai <doit_2017@163.com>
2019-09-08 14:02:03 +0800

    Initial commit

---
+++ Example/SlidingSelector/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  SlidingSelector
+//
+//  Created by sshallow on 09/08/2019.
+//  Copyright (c) 2019 sshallow. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SSViewController : UIViewController
+
+@end
---
+++ Example/SlidingSelector/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SlidingSelector
+//
+//  Created by sshallow on 09/08/2019.
+//  Copyright (c) 2019 sshallow. All rights reserved.
+//
+
+@import UIKit;
+#import "SSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
